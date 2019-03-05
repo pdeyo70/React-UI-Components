f88321c5b168d527c020fc51c9f531de30869681
Paul DeYonghe <p.deyonghe@gmail.com>
2019-03-04 20:53:44 -0600

    MVP of calculator task

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return (
+        <div className="display">
+            <span className="dispText">
+            0
+            </span>
+        </div>
+    )
+}
+
+export default CalculatorDisplay;
